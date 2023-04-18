77a68216a1f18f6ec055028c0e7e4c032f53d726
Rishika Agarwal <122405415+Rishika-0@users.noreply.github.com>
2023-04-18 23:22:06 +0530

    Update Basics 2.py

--- Oops/Basics 2.py
+++ Oops/Basics 2.py
@@ -16,6 +16,15 @@ class employee:
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
+        
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
 
 emp_1 = employee('Rishika','Agarwal',50000)
 emp_2 = employee('Test','User', 60000)
@@ -39,3 +48,18 @@ print(emp_2.raise_amount)
 print(employee.__dict__)
 
 print(employee.num_of_emps)
+emp_1.set_raise_amt(1.06)
+print(employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+emp_str_1 = 'Aditi-Sharma-70000'
+emp_str_2 = 'Vidush-Agarwal-30000'
+emp_str_3 = 'Shivi-Rathod-90000'
+
+first, last, pay = emp_str_1.split('-')
+
+new_emp_1 = employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
